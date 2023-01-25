86e4c4078af57b03d800f3aa05df2e50162be9e4
Mushky Raichik <moussiag@gmail.com>
2023-01-25 14:17:16 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,23 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "tarea 1",
+        description: "descripcion de tarea 1"
+    },
+
+    {
+        id: 1,
+        title: "tarea 2",
+        description: "descripcion de tarea 2"
+    },
+    {
+        id: 2,
+        title: "tarea 3",
+        description: "descripcion de tarea 3"
+    },
+    {
+        id: 3,
+        title: "tarea 4",
+        description: "descripcion de tarea 4"
+    }
+]
